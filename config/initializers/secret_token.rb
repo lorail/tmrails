f5bfc80c4558b3fe60578b0a47286dc46ee289d5
lorail <lori@loriortiz.com>
2013-02-04 14:24:33 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmrails::Application.config.secret_token = 'ea4eca4054da2da25770b70c4fc6c61ccaa0c9925af6009a656e45517cc0188f1e759da2a7cdd9e73c5809510d3a30d7bc24be648b9bf7b1d54c3dfdcb1d5ee9'
