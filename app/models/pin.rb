7783dfe15b9ab0f25160fd328f5f15161b089a99
lorail <lori@loriortiz.com>
2013-02-27 13:51:48 -0500

    Make larger tiles

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,5 +8,5 @@ class Pin < ActiveRecord::Base
  											         size: { less_than: 2.megabytes }
   
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240#" }
+  has_attached_file :image, styles: { medium: "275x275#" }
 end
